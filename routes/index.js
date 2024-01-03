4997f86b0b84190484fa2ddcda0ccb919aded4dd
Smit-107 <smitghevaariya107@gmail.com>
2024-01-03 09:00:42 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var {checkToken} = require('../Middleware/Autho');
+const { Login, Register } = require('../Controller/LoginController');
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'https://student-d-ui.vercel.app/', 
+    // origin:'https://student-data-base.vercel.app/', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+router.use(cors(corsOptions));
+
+/* GET home page. */
+router.post('/Login',Login);
+router.post('/register',Register);
+
+
+module.exports = router;
